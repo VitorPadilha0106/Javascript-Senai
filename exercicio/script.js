f791997c52823511a6b969699a553f4845a5b8c8
VitorPadilha0106 <159196504+VitorPadilha0106@users.noreply.github.com>
2025-06-30 13:46:40 -0300

    terminei

--- exercicio/script.js
+++ exercicio/script.js
@@ -31,17 +31,67 @@ button02.addEventListener("click", (event) => {
 
 const p02 = document.querySelector("#texto")
 const button03 = document.querySelector("#alterar")
-
-
-
+let isButtonOn = false
 
 button03.addEventListener("click", (event) => {
 
-    button03 != false 
+    isButtonOn = !isButtonOn;
 
-    const mudar = button03 == true ? : false
+    p02.style.visibility = isButtonOn ? "visible" : "hidden"
+
+})
+
+// ! Exercicio 04
+
+
+const counter = document.querySelector("#contador")
+const button04 = document.querySelector("#botaoAumentar")
+
+
+button04.addEventListener("click", (event) => {
+
+
+    counter.textContent = Number(counter.textContent) + 1
 
 
 })
 
-    
+
+// ! Exercicio 05
+
+
+const imagens = [
+    "https://media.tenor.com/jwHkGGFNoH8AAAAe/shiny-charmander-pokemon.png",
+    "https://media.tenor.com/Kal1GGUVeD8AAAAe/shiny-charmeleon-pokemon.png",
+    "https://media.tenor.com/ptl9U5YvK_wAAAAe/shiny-charizard-fire.png",
+    "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTPDUDK23FNobuSSd967b5SCYqeKX7ENI1qWzCRmzQyf9WGhY4Y",
+    "https://pm1.aminoapps.com/7271/c8f36f9f3823ec8c1952a0e46fca9bec9db90412r1-478-350v2_hq.jpg",
+    "https://art.pixilart.com/f0dd71c425e45d5.png",
+    "https://art.pixilart.com/thumb/86908fff64d3e0f.png",
+    "https://kandipad.com/assets/images/projects/pp/pp1872-2c67aed6-shiny-ivysaur.png",
+    "https://media.tenor.com/F4f1lhe14lgAAAAe/venusaur-shiny.png"
+];
+
+const img = document.querySelector("#imagem")
+const button05 = document.querySelector("#mudarImagem")
+let contador = 0
+
+img.style.width = "200px"
+
+button05.style.display = "flex"
+button05.style.marginTop = "20px"
+
+button05.addEventListener("click", (event) => {
+
+    contador ++
+
+    if (contador > 8){
+
+        contador = 0
+
+     }
+
+    img.setAttribute("src", imagens[contador])
+
+})
+
